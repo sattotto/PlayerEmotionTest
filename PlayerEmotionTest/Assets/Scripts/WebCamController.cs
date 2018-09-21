03c17db217661af4f4933d9b8b817ad9284345e2
sattotto <sato.fun0619@gmail.com>
2018-09-21 17:05:16 +0900

    add web camera view

---
+++ PlayerEmotionTest/Assets/Scripts/WebCamController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamController : MonoBehaviour {
+
+	public int Width = 640;
+    public int Height = 480;
+    public int FPS = 30;
+
+    void Start () {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        // display all cameras
+        for (var i = 0; i < devices.Length; i++) {
+            Debug.Log (devices [i].name);
+        }
+
+        WebCamTexture webcamTexture = new WebCamTexture(devices[0].name, Width, Height, FPS);
+        GetComponent<Renderer> ().material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+}
